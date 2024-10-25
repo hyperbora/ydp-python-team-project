449700f0b89d2143ff557e830ce6bf181daaaf5e
hyperbora <hyperbora@gmail.com>
2024-10-25 22:16:58 +0900

    fix: pylint 제거

--- main.py
+++ main.py
@@ -3,16 +3,17 @@
 """
 
 import os
+import platform
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-import platform
 
 kivy.require("2.3.0")
 
 # OS별 KIVY_GL_BACKEND 설정
 if platform.system() == "Windows":
     os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
+
 
 class MyFirstKivyApp(App):
     """
